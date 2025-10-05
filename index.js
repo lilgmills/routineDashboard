499d8ae2574e45ff9715eaf323cf74724b7e39e2
lilgmills <laura.a.etale@gmail.com>
2025-10-05 15:09:39 -0500

    server code

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
