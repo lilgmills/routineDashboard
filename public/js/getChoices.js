cda087af3f5ec4b92c28a20a47998d6abe9ea995
lilgmills <laura.a.etale@gmail.com>
2025-10-08 03:14:55 -0500

    fixed lazy generation process and tested even distribution for prng over a year of dates

--- public/js/getChoices.js
+++ public/js/getChoices.js
@@ -10,7 +10,8 @@ export function getChoices(today, items) {
     function createSeededPRNG(seed) {
         return function() {
             // Linear Congruential Generator (LCG)
-            seed = (1664525 * seed + 1013904223) >>> 0;
+            seed = (166401010525 * seed + 101390104223) >>> 0;
+            // console.log("seed updated: ", seed)
             return seed / 0xFFFFFFFF;
         }
     }
@@ -25,7 +26,65 @@ export function getChoices(today, items) {
 
 
     const seed = hashDateString(today);
+    // console.log("day!",today);
+    // console.log("seed!", seed);
     const prng = createSeededPRNG(seed);
+
+    function TEST_PRNG() {
+
+        let test_dict = {}
+
+        for(let items_idx = 0; items_idx < items.length; items_idx++) {
+            test_dict[String(items_idx)] = 0;
+        }
+
+        let dict_days = {"1": 31,
+                 "2": 28,
+                 "3": 31,
+                 "4": 30,
+                 "5": 31,
+                 "6": 30,
+                 "7": 31,
+                 "8": 31,
+                 "9": 30,
+                 "10": 31,
+                 "11": 30,
+                 "12": 31}
+
+        let all_dates = []
+
+        for(let i = 0; i < 12; i++) {
+            let str_m = String(i + 1)
+            for(let day = 0; day < dict_days[str_m]; day++) {
+                all_dates.push(`2025-${str_m.padStart(2, '0')}-${String(day + 1).padStart(2, '0')}`)
+            }
+        }
+
+        console.log('All dates 2025:', all_dates)
+
+        let test_dates = all_dates;
+
+        test_dates.forEach((date)=> {
+            console.log("test date:", date);
+            console.log("hashed date (seed)", hashDateString(date));
+            const rand_num_gen = createSeededPRNG(hashDateString(date))
+            
+            const choices = [];
+            while (choices.length < 3) {
+                let idx = getUniformRandomIndex(items.length, rand_num_gen)
+                console.log("index:", idx)
+                console.log("which is:", items[idx])
+                choices.push(items[idx]);
+                test_dict[String(idx)] += 1;
+            }
+            console.log("sup brah! here are your stretches:", choices);
+        })
+
+        console.log("test_dict distribution:", test_dict)
+
+    }
+
+    // TEST_PRNG();
 
     const choices = [];
     while (choices.length < 3) {
